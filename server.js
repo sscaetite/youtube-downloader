8188598525dfd6e8da6c335f3ac4eb7d56facfd5
Samuel Caetité <sscaetite@gmail.com>
2024-03-14 00:25:05 -0300

    feat(play with no add): add /watch.html route to play video without add

--- server.js
+++ server.js
@@ -37,6 +37,25 @@ app.get("/download-video", async (req, res) => {
   }
 });
 
+// Rota para lidar com a solicitação de download do vídeo
+app.get("/download-video", async (req, res) => {
+  try {
+    const videoUrl = req.query.url;
+
+    if (!videoUrl) {
+      return res.status(400).send("URL do vídeo não fornecida.");
+    }
+
+    const info = await ytdl.getInfo(videoUrl);
+    const format = ytdl.chooseFormat(info.formats, { quality: "highestvideo" });
+    const downloadLink = format.url;
+
+    res.send(downloadLink);
+  } catch (error) {
+    console.error("Erro ao obter o link de download:", error);
+    res.status(500).send("Ocorreu um erro ao obter o link de download.");
+  }
+});
 // Route to handle audio download request
 app.get("/download-audio", async (req, res) => {
   try {
